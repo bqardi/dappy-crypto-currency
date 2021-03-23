5dd86c4c1cd965abf01dd21d9509ea9e4d1b6f62
bqardi <51706945+bqardi@users.noreply.github.com>
2021-03-23 14:27:53 +0100

    CTA to analytics

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
 import "./Button.scss";
 
 function Button({ title }) {
-	return <button className="Button">{title}</button>;
+	return <button
+		// eslint-disable-next-line
+		onClick={() => gtag("event", "cta", {type: "A", cta: title})}
+		className="Button"
+	>
+		{title}
+	</button>;
 }
 
 export default Button;
